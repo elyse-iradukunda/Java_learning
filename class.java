bc979aa3da11c03873999b0cf32b31ec6de32565
Irael <ellycreativity8@gmail.com>
2025-11-05 10:05:07 +0200

    this the codes we used to create java class

---
+++ class.java
@@ -0,0 +1,13 @@
+
+public class Car {
+    String brand;
+    String color;
+    int year;
+    
+    void startEngine(){
+        System.out.println("Engine started!");
+    }
+    void stopEngine(){
+        System.out.println("Engine goes off!");
+    }
+}
